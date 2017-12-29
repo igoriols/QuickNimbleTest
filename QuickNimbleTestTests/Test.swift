cf4af977ed294adc9b25ae57170d7a1871eeceb3
Igor Oliveira <ioliveir@thoughtworks.com>
2017-12-29 15:38:47 -0300

    Initial commit

---
+++ QuickNimbleTestTests/Test.swift
@@ -0,0 +1,22 @@
+//
+//  Test.swift
+//  QuickNimbleTestTests
+//
+//  Created by Igor Oliveira on 29/12/17.
+//  Copyright © 2017 Igor. All rights reserved.
+//
+
+import Nimble
+import Quick
+
+class Test: QuickSpec {
+    override func spec() {
+        context("context") {
+            describe("describe") {
+                it("it") {
+
+                }
+            }
+        }
+    }
+}
